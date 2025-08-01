de9beeb75c7423221fea6607f4a3c4a5034b7918
Raunak <rlpuia@outlook.com>
2025-08-01 21:58:52 +0530

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,36 @@
+import './bootstrap';
+import '../css/app.css';
+
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from "../../vendor/tightenco/ziggy/dist"
+import { Dialog, Loading, Notify, Quasar } from "quasar";
+
+const appName = import.meta.env.VITE_APP_NAME || 'PHED';
+
+createInertiaApp({
+    title: (title) => `${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .use(Quasar,{
+                plugins:{Notify,Dialog,Loading}
+            })
+            .mount(el)
+    },
+    progress: {
+        color: '#4B5563',
+    },
+})
