15e4f51e8ff8590b4bcb5726e949a0e058141a0e
Raunak Msegs <Raunak.msegs@gmail.com>
2025-08-28 14:17:03 +0530

    new build

--- app/Http/Controllers/EditRequestController.php
+++ app/Http/Controllers/EditRequestController.php
@@ -11,27 +11,7 @@ use Illuminate\Http\Request;
 class EditRequestController extends Controller
 {
     //
-    // Submit a new edit request
-//    public function request(Request $request, Employee $model)
-//    {
-//        $validated = $request->validate([
-//            'requested_changes' => 'required|array',
-//        ]);
-//
-//        $changes = collect($validated['requested_changes'])
-//            ->filter(fn($value) => !is_null($value) && $value !== '') // remove empty
-//            ->toArray();
-//
-//        $editRequest = EditRequest::create([
-//            'employee_id' => $model->id,
-//            'requested_changes' => json_encode($changes),
-//            'request_date' => now(),
-//            'approval_status' => 'pending',
-//        ]);
-//
-//        return redirect()->back()->with('success', 'Edit request submitted.');
-//
-//    }
+
     public function request(Request $request, Employee $model)
     {
         $validated = $request->validate([
@@ -131,21 +111,7 @@ class EditRequestController extends Controller
 
 
 
-    // Approve request
-//    public function approve(EditRequest $model)
-//    {
-//        // Apply changes to employee
-//        $model->employee->update(json_decode($model->requested_changes, true));
-//
-//        // Update request status
-//        $model->update([
-//            'approval_status' => 'approved',
-//            'approval_date' => now(),
-//        ]);
-//
-//        return redirect()->back()->with('success', 'Edit request approved.');
-//
-//    }
+
 
     public function approve(EditRequest $model)
     {
---
+++ public/build/assets/use-quasar-div2WQT7.js
@@ -0,0 +1,1 @@
+import{K as a,b6 as r}from"./app-B0tffRyF.js";function u(){return a(r)}export{u};
