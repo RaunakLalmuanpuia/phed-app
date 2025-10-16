c477da7312d230e3abf6080875e5b819fa68a669
Raunak Msegs <Raunak.msegs@gmail.com>
2025-10-16 11:18:26 +0530

    APi route bug

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\API\EmployeeBioController;
+use App\Http\Controllers\Api\EmployeeBioController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
